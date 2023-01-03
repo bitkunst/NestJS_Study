77ffe2f346a1d9d91a9d93714563a5f83f61a826
bitkunst <bitkunst23@gmail.com>
2023-01-03 23:47:43 +0900

    [230103_study] feat: Rest api

--- 1_express_for_NestJS/src/app.ts
+++ 1_express_for_NestJS/src/app.ts
@@ -1,47 +1,27 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.route';
 
 const app: express.Express = express();
 const port: number = 8000;
 
-// middleware
+//* logging middleware
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.url, req.rawHeaders[1]);
+    console.log(req.rawHeaders[1]);
     console.log('this is logging middleware');
     next();
 });
 
-app.get('/cats/som', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('this is som middleware');
-    next();
-});
+//* JSON middleware
+app.use(express.json());
 
-app.get('/cats', (req: express.Request, res: express.Response) => {
-    // console.log(req.url, req.rawHeaders[1]);
-    res.json({
-        cats: Cat,
-    });
-});
+//* router
+app.use(catsRouter);
 
-app.get('/cats/blue', (req: express.Request, res: express.Response) => {
-    // console.log(req.url, req.rawHeaders[1]);
-    res.json({
-        blue: Cat[0],
-    });
-});
-
-app.get('/cats/som', (req: express.Request, res: express.Response) => {
-    // console.log(req.url, req.rawHeaders[1]);
-    res.json({
-        som: Cat[1],
-    });
-});
-
-// 404 error middleware
+//* 404 error middleware
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log('this is error middleware');
-    res.status(404).send({
-        error: '404 Not Found Error',
+    res.status(404).json({
+        error: '404 Not Found error',
     });
 });
 
